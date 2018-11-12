4ac9a35ae957283d19568d943a519c1147b03809
Unknown <daniel.bh38@gmail.com>
2018-11-12 20:59:49 +0200

    enable_policy
    implement "enable_policy" wrapper (hw1_syscalls.h)
    implement "sys_call_enable_policy" system call(hw1_syscalls.c)
    changed task_struct ( sched.h) - now it contains also :
    	int privilege_level;
    	forbidden_activity_info* log_array;
    	int feature_status; //1 enable 0 disable
    	int size_log_array;
    	int num_of_error;  
    in sched.h we added also the struct:
    struct forbidden_activity_info {
    int syscall_req_level;
    int proc_level;
    int time;
    };
    typedef struct forbidden_activity_info forbidden_activity_info;

---
+++ hw1_syscalls.c
@@ -0,0 +1,44 @@
+#include <linux/sched.h>
+#include <linux/slab.h>
+#include <asm/uaccess.h>
+#include <linux/kernel.h>
+
+int sys_enable_policy(pid_t pid, int size, int password){
+	int res;
+	
+	//parameters tests
+	if(pid<0){
+		res = -ESRCH;
+		return res;
+	}
+	task_t* task = find_task_by_pid(pid);//pointer to task
+	if(!task){
+		res = -ESRCH;
+		return res;
+	}
+	if(password != 234123){
+		res = -EINVAL;
+		return res;
+	}
+	if(task->feature_status == 1){
+		res = -EINVAL;
+		return res;
+	}
+	if(size<0){
+		res = -EINVAL;
+		return res;
+	}
+	
+	task->log_array = kmalloc(sizeof(*(task->log_array))*size,GFP_KERNEL);
+	if(!task->log_array){
+		res = -ENOMEM;
+		return res;
+	}
+	task->size_log_array = size;
+	task->num_of_error = 0;
+	task->privilege_level = 2;
+	task->feature_status=1;
+	
+	res = 0;
+	return res;
+}
