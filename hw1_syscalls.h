84fb03219f4ad4066328d8dbef164c172ac39eb7
Unknown <daniel.bh38@gmail.com>
2018-11-14 16:08:13 +0200

    disable_policy
    added sys_call of disabled policy , and new line in entry.S

--- hw1_syscalls.h
+++ hw1_syscalls.h
@@ -24,3 +24,20 @@ int enable_policy(pid_t pid, int size, int password){
 	
 	return res;
 }
+
+int disable_policy(pid_t pid , int password){
+	unsigned int res;
+	__asm__ (
+	    "int $0x80;"
+		: "=a" (res)
+		: "0" (244) ,"b" (pid) ,"c" (password) 
+		: "memory"
+	);
+	
+	if((res)<0){
+		errno = -(res);
+		return -1;
+	}
+	
+	return res;
+}
